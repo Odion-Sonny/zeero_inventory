69628fc3b675a7dfc0f6f0ba50f92f7b4aa5fb22
Odion Sonny-Egbeahie <99491084+Odion-Sonny@users.noreply.github.com>
2024-09-19 15:06:43 +0100

    undate settings.py

--- zeero_inventory/settings.py
+++ zeero_inventory/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2b-29n(65#a89m@q=b@f6r9k(x+l^cxqxczs5q#i91ceibsvkn'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*'] # just for testing purposes
 
 
 # Application definition
